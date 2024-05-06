3237c4a5b01ca18b424464a05857a3c3d2373b00
HelgGhost <HelgGhost@yandex.ru>
2024-05-06 07:17:12 +0700

    3

--- manager-app/src/main/java/ag/selm/manager/controller/ProductsController.java
+++ manager-app/src/main/java/ag/selm/manager/controller/ProductsController.java
@@ -5,6 +5,7 @@ import ag.selm.manager.client.ProductsRestClient;
 import ag.selm.manager.controller.payload.NewProductPayload;
 import ag.selm.manager.entity.Product;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,7 +24,8 @@ public class ProductsController {
     private final ProductsRestClient productsRestClient;
 
     @GetMapping("list")
-    public String getProductsList(Model model, @RequestParam(name = "filter", required = false) String filter) {
+    public String getProductsList(Model model, @RequestParam(name = "filter", required = false) String filter,
+                                  Principal principal) {
         model.addAttribute("products", this.productsRestClient.findAllProducts(filter));
         model.addAttribute("filter", filter);
         return "catalogue/products/list";
